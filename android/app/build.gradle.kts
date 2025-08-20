1ecdd34c570627037c7bf74c013ad58a55f3e9df
piyumi27 <piyurajakaruna@gmail.com>
2025-08-20 15:02:33 +0530

    Fixed Package Name Mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.aayu"
-    compileSdk = 34
+    namespace = "dev.aayu"
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.aayu"
+        applicationId = "dev.aayu"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/dev/aayu/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.aayu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
